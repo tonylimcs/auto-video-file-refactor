933415f6ecd4bc3042e67cc6d8f10b63233ff232
Lim Cheng Siang <cslim.careers@gmail.com>
2024-12-04 04:51:04 +0800

    Update test_autosuggest.py

--- tests/test_autosuggest.py
+++ tests/test_autosuggest.py
@@ -21,9 +21,17 @@ def test_similarity_fail(filenames, expected):
     assert similarity(filenames)[0] == expected
 
 
-def test_autosuggest_video_title(tmp_path):
+def test_autosuggest_video_title_with_year(tmp_path):
     d = tmp_path / "This.is.a.Movie.(2024).blah.blah.blah"
     d.mkdir()
     p = d / "This.is.a.Movie.(2024).blah.blah.blah.mkv"
     p.write_text("")
     assert autosuggest_movie_title(str(d)) == "This Is A Movie (2024)"
+
+
+def test_autosuggest_video_title_without_year(tmp_path):
+    d = tmp_path / "This.is.a.Movie.blah.blah.blah"
+    d.mkdir()
+    p = d / "This.is.a.Movie.blah.blah.blah.mkv"
+    p.write_text("")
+    assert autosuggest_movie_title(str(d)) == "This.is.a.Movie.blah.blah.blah"
