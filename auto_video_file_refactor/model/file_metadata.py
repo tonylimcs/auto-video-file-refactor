f60d08e8aaaf790ce990cd8fc785fac9b7b96fe1
Lim Cheng Siang <cslim.careers@gmail.com>
2024-12-10 08:24:33 +0800

    Naming convention

--- auto_video_file_refactor/model/file_metadata.py
+++ auto_video_file_refactor/model/file_metadata.py
@@ -7,22 +7,22 @@ import re
 
 @dataclass
 class FileMetadata:
-    __path: str | bytes
+    _path: str | bytes
     name: str = field(init=False)
     ext: str = field(init=False)
     type: str = field(init=False)
     dir: str = field(init=False)
 
     def __post_init__(self):
-        self.__path = os.path.normpath(self.__path)
-        self.ext = common.get_file_ext(self.__path)
-        self.name = common.get_file_title(self.__path, self.ext)
+        self._path = os.path.normpath(self._path)
+        self.ext = common.get_file_ext(self._path)
+        self.name = common.get_file_title(self._path, self.ext)
         self.type = common.FILE_FOLDER if not self.ext else self.ext
-        self.dir = os.path.dirname(self.__path)
+        self.dir = os.path.dirname(self._path)
 
     @property
     def path(self) -> str:
-        return self.__path
+        return self._path
 
     @property
     def basic(self) -> tuple:
@@ -31,8 +31,8 @@ class FileMetadata:
 
 @dataclass
 class VideoFileMetadata(FileMetadata):
-    __root_dir: str
-    __title: str
+    _root_dir: str
+    _title: str
     season: str = field(default="", init=False)
     episode: str = field(default="", init=False)
 
@@ -46,11 +46,11 @@ class VideoFileMetadata(FileMetadata):
 
     @property
     def root_dir(self) -> str:
-        return os.path.normpath(self.__root_dir)
+        return os.path.normpath(self._root_dir)
 
     @property
     def title(self) -> str:
-        return self.__title
+        return self._title
 
 
 @dataclass
@@ -62,7 +62,7 @@ class Refactored:
         season: str
         file: str
 
-    __raw: VideoFileMetadata
+    _raw: VideoFileMetadata
     name: str = field(init=False)
     path: Path = field(init=False)
 
@@ -71,14 +71,14 @@ class Refactored:
         self.path = self.refactor_path()
 
     def refactor_name(self) -> str:
-        series = f" S{self.__raw.season}E{self.__raw.episode}" if self.__raw.season and self.__raw.episode else ""
-        return f"{self.__raw.title}{series}{self.__raw.ext}"
+        series = f" S{self._raw.season}E{self._raw.episode}" if self._raw.season and self._raw.episode else ""
+        return f"{self._raw.title}{series}{self._raw.ext}"
 
     def refactor_path(self) -> Path:
-        title_path = os.path.join(self.__raw.root_dir, self.__raw.title)
+        title_path = os.path.join(self._raw.root_dir, self._raw.title)
 
-        season_name = f"Season {int(self.__raw.season)}" if self.__raw.season else ""
-        season_path = os.path.join(title_path, season_name) if self.__raw.season else ""
+        season_name = f"Season {int(self._raw.season)}" if self._raw.season else ""
+        season_path = os.path.join(title_path, season_name) if self._raw.season else ""
 
         file_path = os.path.join(title_path, season_name, self.name)
 
@@ -86,4 +86,4 @@ class Refactored:
 
     @property
     def raw(self) -> VideoFileMetadata:
-        return self.__raw
+        return self._raw
